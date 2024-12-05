685338dd66cec125e9341c6331b09c68efe969f9
Louise Delarue <louise.delarue@epita.fr>
2024-12-05 12:07:36 +0100

    test

--- space/space_invaders_TP1.asm
+++ space/space_invaders_TP1.asm
@@ -67,7 +67,7 @@ HLines			movem.l d6/d7/a0,-(a7)
 				rts
 
 WhiteSquare32	movem 	d7-d5/a0,-(a7)
-				lea 	VIDEO_START+((BYTE_PER_LINE-4)/2) + ((VIDEO_HEIGHT-32)/2) + ((VIDEO_WIDTH-32)/2),a0
+				lea VIDEO_START+((BYTE_PER_LINE-4)/2)+(((VIDEO_HEIGHT-32)/2)*BYTE_PER_LINE),a0
 				;movea.l	#VIDEO_START,a01
 				;move.l 	#VIDEO_SIZE,d7
 				;move.l 	d6,VIDEO_HEIGHT/2-16
@@ -77,7 +77,70 @@ WhiteSquare32	movem 	d7-d5/a0,-(a7)
 				adda.l	#BYTE_PER_LINE,a0
 				dbra 	d7,\loop
 				movem 	(a7)+,d7/a0
+				rts
 
+WhiteSquare128 	movem.l d7/a0,-(a7)
+				lea 	VIDEO_START+((BYTE_PER_LINE-16)/2)+(((VIDEO_HEIGHT-128)/2)*BYTE_PER_LINE),a0
+				move.w #128-1,d7
+				
+\loop			move.l #$ffffffff,(a0)
+				move.l #$ffffffff,4(a0)
+				move.l #$ffffffff,8(a0)
+				move.l #$ffffffff,12(a0)
+				adda.l #BYTE_PER_LINE,a0
+				dbra d7,\loop
+				movem.l (a7)+,d7/a0
+				rts
+
+WhiteLine		movem.l	d0/a0,-(a7)
+				subq.w #1,d0
+
+\loop 			move.b #$ff,(a0)+
+				dbra d0,\loop
+				
+				movem.l (a7)+,d0/a0
+				rts
+				
+WhiteSquare		movem.l d0-d2/a0,-(a7)
+				move.w 	d0,d2
+				lsl.w 	#3,d2
+				lea 	VIDEO_START,a0
+				move.w 	#BYTE_PER_LINE,d1
+				sub.w 	d0,d1
+				lsr.w 	#1,d1	
+				adda.w 	d1,a0
+				move.w 	#VIDEO_HEIGHT,d1
+				sub.w 	d2,d1
+				lsr.w 	#1,d1
+				mulu.w 	#BYTE_PER_LINE,d1
+				adda.w 	d1,a0
+				subq.w 	#1,d2
+				
+\loop			jsr 	WhiteLine
+				adda.l 	#BYTE_PER_LINE,a0
+				dbra 	d2,\loop
+				movem.l (a7)+,d0-d2/a0
+				rts
+				
+Invader_Bitmap 	dc.b %00001100,%00000000,%11000000
+				dc.b %00001100,%00000000,%11000000
+				dc.b %00000011,%00000011,%00000000
+				dc.b %00000011,%00000011,%00000000
+				dc.b %00001111,%11111111,%11000000
+				dc.b %00001111,%11111111,%11000000
+				dc.b %00001100,%11111100,%11000000
+				dc.b %00001100,%11111100,%11000000
+				dc.b %00111111,%11111111,%11110000
+				dc.b %00111111,%11111111,%11110000
+				dc.b %11001111,%11111111,%11001100
+				dc.b %11001111,%11111111,%11001100
+				dc.b %11001100,%00000000,%11001100
+				dc.b %11001100,%00000000,%11001100
+				dc.b %00000011,%11001111,%00000000
+				dc.b %00000011,%11001111,%00000000
+
+NewInvaderShot	
+				
 			; ==============================
 			; Données
 			; ==============================
